64e0080c36dc4804469100d2e18b759452639f13
wendryuslima <141366401+wendryuslima@users.noreply.github.com>
2026-03-07 21:44:30 -0300

    chore: add better auth setup

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import "dotenv/config";
 
+import fastifyCors from "@fastify/cors";
 import fastifySwagger from "@fastify/swagger";
 import fastifySwaggerUI from "@fastify/swagger-ui";
 import Fastify from "fastify";
@@ -10,6 +11,8 @@ import {
   ZodTypeProvider,
 } from "fastify-type-provider-zod";
 import { z } from "zod/v4";
+
+import { auth } from "./lib/auth.js";
 
 const app = Fastify({
   logger: true,
@@ -36,6 +39,11 @@ await app.register(fastifySwaggerUI, {
   routePrefix: "/docs",
 });
 
+await app.register(fastifyCors, {
+  origin: ["localhost:3000"],
+  credentials: true,
+});
+
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 app.withTypeProvider<ZodTypeProvider>().route({
@@ -58,6 +66,38 @@ app.withTypeProvider<ZodTypeProvider>().route({
   },
 });
 
+app.route({
+  method: ["GET", "POST"],
+  url: "/api/auth/*",
+  async handler(request, reply) {
+    try {
+      const url = new URL(request.url, `http://${request.headers.host}`);
+
+      const headers = new Headers();
+      Object.entries(request.headers).forEach(([key, value]) => {
+        if (value) headers.append(key, value.toString());
+      });
+
+      const req = new Request(url.toString(), {
+        method: request.method,
+        headers,
+        ...(request.body ? { body: JSON.stringify(request.body) } : {}),
+      });
+
+      const response = await auth.handler(req);
+
+      reply.status(response.status);
+      response.headers.forEach((value, key) => reply.header(key, value));
+      reply.send(response.body ? await response.text() : null);
+    } catch (error) {
+      app.log.error(error);
+      reply.status(500).send({
+        error: "Internal authentication error",
+        code: "AUTH_FAILURE",
+      });
+    }
+  },
+});
 try {
   await app.listen({ port: Number(process.env.PORT || 8081) });
 } catch (err) {
