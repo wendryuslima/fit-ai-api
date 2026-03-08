5d5871731c1822339f32a1c29fb1962e5716a773
wendryuslima <141366401+wendryuslima@users.noreply.github.com>
2026-03-08 15:01:27 -0300

    feat: add create workou plan endpoint

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,71 @@ import "dotenv/config";
 import fastifyCors from "@fastify/cors";
 import fastifySwagger from "@fastify/swagger";
 import fastifyApiReference from "@scalar/fastify-api-reference";
+import { fromNodeHeaders } from "better-auth/node";
 import Fastify from "fastify";
 import {
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
   ZodTypeProvider,
 } from "fastify-type-provider-zod";
 import { z } from "zod/v4";
 
+import { NotFoundError } from "./errors/index.js";
+import { WeekDay } from "./generated/prisma/enums.js";
 import { auth } from "./lib/auth.js";
+import { CreateWorkoutPlan } from "./uscases/CreateWorkoutPlan.js";
 
 const app = Fastify({
   logger: true,
 });
+
+const createWorkoutPlanBodySchema = z
+  .object({
+    name: z.string().trim().min(1),
+    workoutDays: z.array(
+      z.object({
+        name: z.string().trim().min(1),
+        weekDay: z.enum(WeekDay),
+        isRest: z.boolean().default(false),
+        estimatedDurationInSeconds: z.number().min(1),
+        exercises: z.array(
+          z.object({
+            order: z.number().min(0),
+            name: z.string().trim().min(1),
+            sets: z.number().min(1),
+            reps: z.number().min(1),
+            restTimeInSeconds: z.number().min(1),
+          }),
+        ),
+      }),
+    ),
+  });
+
+const createWorkoutPlanResponseSchema = z.object({
+  id: z.uuid(),
+  name: z.string().trim().min(1),
+  workoutDays: z.array(
+    z.object({
+      name: z.string().trim().min(1),
+      weekDay: z.enum(WeekDay),
+      isRest: z.boolean().default(false),
+      estimatedDurationInSeconds: z.number().min(1),
+      exercises: z.array(
+        z.object({
+          order: z.number().min(0),
+          name: z.string().trim().min(1),
+          sets: z.number().min(1),
+          reps: z.number().min(1),
+          restTimeInSeconds: z.number().min(1),
+        }),
+      ),
+    }),
+  ),
+});
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
 
 await app.register(fastifySwagger, {
   openapi: {
@@ -24,6 +76,7 @@ await app.register(fastifySwagger, {
       version: "1.0.0",
     },
   },
+  transform: jsonSchemaTransform,
 });
 
 await app.register(fastifyCors, {
@@ -50,6 +103,68 @@ await app.register(fastifyApiReference, {
 });
 
 app.withTypeProvider<ZodTypeProvider>().route({
+  method: "POST",
+  url: "/workout-plans",
+  schema: {
+    body: createWorkoutPlanBodySchema,
+    response: {
+      201: createWorkoutPlanResponseSchema,
+      400: z.object({
+        error: z.string(),
+        code: z.string(),
+      }),
+      401: z.object({
+        error: z.literal("Unauthorized"),
+        code: z.literal("UNAUTHORIZED"),
+      }),
+      404: z.object({
+        error: z.string(),
+        code: z.string(),
+      }),
+      500: z.object({
+        error: z.string(),
+        code: z.string(),
+      }),
+    },
+  },
+
+  handler: async (request, reply) => {
+    try {
+      const session = await auth.api.getSession({
+        headers: fromNodeHeaders(request.headers),
+      });
+
+      if (!session) {
+        return reply.status(401).send({
+          error: "Unauthorized",
+          code: "UNAUTHORIZED",
+        });
+      }
+      const createWorkoutPlan = new CreateWorkoutPlan();
+      const result = await createWorkoutPlan.execute({
+        userId: session.user.id,
+        name: request.body.name,
+        workoutDays: request.body.workoutDays,
+      });
+
+      return reply.status(201).send(result);
+    } catch (error) {
+      if (error instanceof NotFoundError) {
+        return reply.status(404).send({
+          error: error.message,
+          code: "NOT_FOUND_ERROR",
+        });
+      }
+      app.log.error(error);
+      return reply.status(500).send({
+        error: "Internal server error",
+        code: "INTERNAL_SERVER_ERROR",
+      });
+    }
+  },
+});
+
+app.withTypeProvider<ZodTypeProvider>().route({
   method: "GET",
   url: "/swagger.json",
   schema: {
@@ -60,8 +175,6 @@ app.withTypeProvider<ZodTypeProvider>().route({
   },
 });
 
-app.setValidatorCompiler(validatorCompiler);
-app.setSerializerCompiler(serializerCompiler);
 app.withTypeProvider<ZodTypeProvider>().route({
   method: "GET",
   url: "/",
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,13 +1,9 @@
-import { PrismaPg } from "@prisma/adapter-pg";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { openAPI } from "better-auth/plugins";
 
-import { PrismaClient } from "../generated/prisma/client.js";
+import { prisma } from "./db.js";
 
-const prisma = new PrismaClient({
-  adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL }),
-});
 export const auth = betterAuth({
   trustedOrigins: ["localhost:3000"],
   emailAndPassword: {
