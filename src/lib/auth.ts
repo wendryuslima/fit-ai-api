1e11b63d4fe06929b257498aac6cca1345abd8cf
wendryuslima <141366401+wendryuslima@users.noreply.github.com>
2026-03-19 21:20:21 -0300

    feat: add signin with google

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -5,9 +5,13 @@ import { openAPI } from "better-auth/plugins";
 import { prisma } from "./db.js";
 
 export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL,
   trustedOrigins: ["localhost:3000"],
-  emailAndPassword: {
-    enabled: true,
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
   },
   database: prismaAdapter(prisma, {
     provider: "postgresql",
